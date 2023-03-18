5bbbf6b69b5503593a524d3f1eddf455e6d65c49
estradax <azharumuhammad@gmail.com>
2023-03-18 21:42:28 +0700

    remove default featured option template

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -56,15 +56,6 @@
                             @endforeach
                         </ul>
                     </div>
-                    <div class="col-md-6 pb-4">
-                        <div class="d-flex">
-                            <select class="form-control">
-                                <option>Featured</option>
-                                <option>A to Z</option>
-                                <option>Item</option>
-                            </select>
-                        </div>
-                    </div>
                 </div>
                 <div class="row">
                     @foreach($robots as $robot)
