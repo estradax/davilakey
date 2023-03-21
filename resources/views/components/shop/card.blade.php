e8f8cb7c15505e213dbe09bf94076928528f2881
estradax <azharumuhammad@gmail.com>
2023-03-21 22:17:50 +0700

    remove unnecessary image overlay

--- resources/views/components/shop/card.blade.php
+++ resources/views/components/shop/card.blade.php
@@ -2,13 +2,6 @@
     <div class="card mb-4 product-wap rounded-0">
         <div class="card rounded-0">
             <img style="width: 600px !important; margin-left: 0 !important; max-width: 100% !important; height: 400px !important; object-fit: cover !important;" class="card-img rounded-0 img-fluid" src="{{ $robot->image_url }}">
-            <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
-                <ul class="list-unstyled">
-                    <li><a class="btn btn-success text-white" href="shop-single.html"><i class="far fa-heart"></i></a></li>
-                    <li><a class="btn btn-success text-white mt-2" href="shop-single.html"><i class="far fa-eye"></i></a></li>
-                    <li><a class="btn btn-success text-white mt-2" href="shop-single.html"><i class="fas fa-cart-plus"></i></a></li>
-                </ul>
-            </div>
         </div>
         <div class="card-body">
             <a href="{{ route('shop.show', $robot->id) }}" class="h3 text-decoration-none">{{ $robot->name }}</a>
