398460b8d33f498dacacea43987e9d5df9223bef
estradax <azharumuhammad@gmail.com>
2023-05-06 16:12:39 +0700

    dynamically bump copyright year

--- resources/views/components/shell/footer.blade.php
+++ resources/views/components/shell/footer.blade.php
@@ -78,7 +78,7 @@
             <div class="row pt-2">
                 <div class="col-12">
                     <p class="text-left text-light">
-                        Copyright &copy; 2023 {{ config('app.name') }}
+                        Copyright &copy; {{ \Carbon\Carbon::now()->format('Y') }} {{ config('app.name') }}
                     </p>
                 </div>
             </div>
