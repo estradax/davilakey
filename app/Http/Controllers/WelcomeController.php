69bd87502b55f772b0fc738309deeba0bc5fd122
estradax <azharumuhammad@gmail.com>
2023-05-07 19:37:02 +0700

    add cloudinary in a good way

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,23 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Robot;
+use Cloudinary\Transformation\Resize;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 class WelcomeController extends Controller
 {
-    public function __invoke($media)
+    public function __invoke()
     {
         $categories = [
             [
                 'name' => 'Industrial',
-                'image_url' => '/assets/robot-categories/sparepart.jpg'
+                'image_url' => Cloudinary::getImage('industrial_ovpxmy')
+                    ->resize(Resize::scale()->width(400)->height(400))->toUrl(),
             ],
             [
                 'name' => 'Spare parts',
-                'image_url' => '/assets/robot-categories/arduino.jpg'
+                'image_url' => Cloudinary::getImage('arduino_lpqzv3')
+                    ->resize(Resize::scale()->width(400)->height(400))->toUrl(),
             ],
             [
                 'name' => 'The one',
-                'image_url' => '/assets/robot-categories/tesla.jpg'
+                'image_url' => Cloudinary::getImage('tesla_hzvrgi')
+                    ->resize(Resize::scale()->width(400)->height(400))->toUrl(),
             ]
         ];
 
