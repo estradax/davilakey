74c9b3569f691a91cb627d6c4b8ae8eee2dcac62
azharjk <azhr.project@gmail.com>
2023-07-18 07:14:31 +0700

    feat: add protobuf

--- proto-gen.sh
+++ proto-gen.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-protoc --go_out=paths=source_relative:. proto/product/product.proto
+PHP_OUT=packages/davilakey
+
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/product/product.proto
+protoc --php_out=${PHP_OUT} --grpc_out=generate_server:${PHP_OUT} --plugin=protoc-gen-grpc=$(printenv GRPC_PHP_PLUGIN) proto/product/product.proto
